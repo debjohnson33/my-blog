9e3236f4444cc2492d36091e152112e3170644df
debjohnson33 <debjohnson_11@yahoo.com>
2019-07-21 14:49:35 -0500

    Adds react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage';
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <Router>
+      <div className="App">
+        <Route path='/' component={HomePage} exact />
+      </div>
+    </Router>
   );
 }
 
