90166e95ab3cba224eb7f750c419da13ca869edc
debjohnson33 <debjohnson_11@yahoo.com>
2019-07-21 15:05:10 -0500

    Makes routes for About/Article/ArticlesList

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlesList from './pages/ArticlesList';
+import ArticlePage from './pages/ArticlePage';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Route path='/' component={HomePage} exact />
+        <div id="page-body">
+          <Route path='/' component={HomePage} exact />
+          <Route path='/about' component={AboutPage} />
+          <Route path='/articles-list' component={ArticlesList} />
+          <Route path='/article' component={ArticlePage} />
+        </div>
+        
       </div>
     </Router>
   );
