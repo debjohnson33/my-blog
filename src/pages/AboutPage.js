f5a9c839bae639759e153fc32f9e55810dcd98de
debjohnson33 <debjohnson_11@yahoo.com>
2019-07-21 14:56:19 -0500

    Adds AboutPage/ArticlesList/ArticlesPage

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const AboutPage = () => (
+    <> 
+        <h1>About Me</h1>
+        <p>
+            Welcome to my blog! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id ligula quis eros molestie sagittis a vel urna. In blandit nibh enim, et venenatis erat scelerisque et. Phasellus est ipsum, sagittis sed ultricies sit amet, laoreet ac metus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Ut efficitur quam sit amet nibh vestibulum, quis convallis lectus aliquam. Integer vitae leo et urna vestibulum tristique id sed dolor. Sed porta velit a turpis efficitur congue. Nulla ac faucibus massa. Nullam in leo ac eros vulputate efficitur eu in nulla. Vestibulum porttitor tellus ac lacinia pulvinar. Duis eu tempor risus. Etiam tempus blandit ligula a bibendum. Nunc vitae dapibus nulla, vel porttitor dui. Nullam eu libero vel ex ullamcorper posuere a aliquet odio.
+        </p>
+        <p>
+        Nullam vitae eros in risus aliquam vehicula a et ex. Maecenas vehicula libero elit, non imperdiet elit ultrices eget. Suspendisse et sapien id arcu blandit vehicula nec quis urna. Maecenas vestibulum tempor quam, a sodales enim blandit at. Proin sed tempus ligula. Curabitur semper purus nec odio faucibus rhoncus. Proin cursus, lacus ut sollicitudin convallis, nibh mauris pellentesque nisl, ac consectetur nibh ligula eget augue. Sed cursus mattis enim, ut tempor nibh gravida in. Mauris sed dui at enim tincidunt molestie quis lacinia nisl. Proin mollis vitae eros id accumsan. 
+        </p>
+    </>
+)
+
+export default AboutPage;
